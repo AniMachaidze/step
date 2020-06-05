15c2169b4132ac38afc013368e42a3050bcbd352
Ani Machaidze <animachaidze@gmail.com>
2020-06-05 22:34:53 +0000

    Add function for deleting specific comment

--- portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
@@ -12,6 +12,11 @@ import com.google.appengine.api.datastore.KeyFactory;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.datastore.Query.CompositeFilter;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,16 +28,29 @@ public class DeleteDataServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String page = request.getParameter("page");
+        String text = request.getParameter("text");
+        String author = request.getParameter("author");
+        String emotion = request.getParameter("emotion");
+
         Query query = new Query("Comment-" + page);
+
+        if (!text.equals("undefined")) {
+            Filter textPropertyFilter = new FilterPredicate("text", FilterOperator.EQUAL, text);
+            Filter authorPropertyFilter = new FilterPredicate("author", FilterOperator.EQUAL, author);
+            Filter emotionPropertyFilter = new FilterPredicate("emotion", FilterOperator.EQUAL, emotion);
+            CompositeFilter filter =  CompositeFilterOperator.and(textPropertyFilter, authorPropertyFilter, emotionPropertyFilter);
+            query.setFilter(filter);
+        } 
+
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         PreparedQuery results = datastore.prepare(query);
+
         List<Key> keys = new ArrayList<>();
         for (Entity entity : results.asIterable()) {
             keys.add(entity.getKey());
         }
 
         datastore.delete(keys);
-
         response.setContentType("text/plain");
         response.getWriter().println("");
     }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -29,61 +29,79 @@ function getComments() {
 			comments.forEach((comment) => {
 				let date = new Date(comment.date);
 				commentListElement.appendChild(
-					createListElement(comment.author, 
+					createListElement(comment.author,
 						date.getMonth() + '/' + date.getDate() + '/' +
 						date.getFullYear(), comment.content, comment.emotion));
 			})
-	    });
+		});
 }
 
 /**
  * Fetches delete-data, deletes all commennts
  */
-function deleteComments() {
+function deleteComments(author, date, text, emotion) {
 	const pageEl = document.getElementById("page");
 	const page = pageEl.value;
-	fetch('/delete-data?page=' + page, {
-		method: 'POST'
+	const queryStr = 'page=' + page + '&' +
+		'author=' + author + '&' +
+		'date=' + date + '&' +
+		'text=' + text + '&' +
+		'emotion=' + emotion;
+	fetch('/delete-data?' + queryStr, {
+		method: 'POST',
 	});
 
-	document.getElementById('comments-container').innerHTML = '';
+	if (text == 'undefined') {
+		document.getElementById('comments-container').innerHTML = '';
+	} else {
+		getComments();
+	}
+
 }
 
-/** Creates an <li> element containing text. */
+/** Creates an <li> element containing comment data: author, date, text and emotion emoji. */
 function createListElement(author, date, text, emotion) {
 	const liElement = document.createElement('li');
 
-    const emotionEl = document.createElement('div');
-    switch(emotion) {
-    case 'happy':
-        emotionEl.innerHTML = '&#128522; ';
-        break;
-    case 'laughting':
-        emotionEl.innerHTML =  '&#128516; ';
-        break;
-    case 'surprised':
-        emotionEl.innerHTML = '&#128562; ';
-        break;
-    case 'sad':
-        emotionEl.innerHTML = '&#128532; ';
-        break;
-    default:
-        emotionEl.innerHTML = '&#128522; ';
-    }
+	const containerDiv = document.createElement('div');
 
+	const emotionEl = document.createElement('div');
+	switch (emotion) {
+		case 'happy':
+			emotionEl.innerHTML = '&#128522; ';
+			break;
+		case 'laughing':
+			emotionEl.innerHTML = '&#128516; ';
+			break;
+		case 'surprised':
+			emotionEl.innerHTML = '&#128562; ';
+			break;
+		case 'sad':
+			emotionEl.innerHTML = '&#128532; ';
+			break;
+		default:
+			emotionEl.innerHTML = '&#128522; ';
+	}
 
-    const bold = document.createElement('b');
-    bold.innerText = author;
-    emotionEl.appendChild(bold);
-    const linebreak = document.createElement('br');
-    liElement.appendChild(emotionEl);
-    liElement.appendChild(linebreak);
-    const dateNode = document.createElement('i');
-    dateNode.innerText = date;
-    liElement.appendChild(dateNode);
-    liElement.appendChild(linebreak);
-    const textNode = document.createTextNode(text);
-    liElement.appendChild(textNode);
+	const deleteButton = document.createElement('button');
+	deleteButton.innerHTML = '&#10005;';
+	deleteButton.className = "delete-button";
+	deleteButton.onclick = function () {
+		deleteComments(author, date, text, emotion);
+	}
+
+	containerDiv.className = 'comment-container';
+	containerDiv.appendChild(emotionEl);
+	const authorText = document.createElement('b');
+	authorText.innerText = author;
+	emotionEl.appendChild(authorText);
+	emotionEl.appendChild(deleteButton);
+	const dateNode = document.createElement('i');
+	dateNode.innerText = date;
+	containerDiv.appendChild(dateNode);
+	liElement.appendChild(containerDiv);
+	const textNode = document.createTextNode(text);
+	liElement.appendChild(textNode);
 
 	return liElement;
 }
