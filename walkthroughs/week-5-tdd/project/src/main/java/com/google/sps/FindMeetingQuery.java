ad68d7af3a5aed64eb26f4bfb2ee467b9a38decc
Ani Machaidze <animachaidze@gmail.com>
2020-06-19 22:04:39 +0000

    Add comments and more descriptive variables

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -54,7 +54,7 @@ public final class FindMeetingQuery {
     List<TimeRange> busyTimes = new ArrayList<>();
     Collection<TimeRange> freeTimes;
 
-    // finds time ranges that does not work for at least one required attendee
+    // finds time ranges that do not work for at least one required attendee
     for (Event event : events) {
       TimeRange eventTimeRange = event.getWhen();
 
@@ -64,13 +64,15 @@ public final class FindMeetingQuery {
       }
     }
 
-    Collections.sort(busyTimes, TimeRange.ORDER_BY_START);
     freeTimes = findFreeTimesFromBusy(busyTimes, duration);
 
     return freeTimes;
   }
 
-  /** Calculates for each minute in a day how many optional attendee are free */
+  /**
+   * Calculates for each minute in a day how many optional attendee are free. Returns list X of size
+   * 1440, where X[i] is how many optional attendees are free on minute i
+   */
   private List<Integer> getAvailableTimesForOptionals(
       Collection<String> optionalAttendees, Collection<Event> events, int duration) {
     Map<TimeRange, Set<String>> busyTimesForOptionals = new HashMap<>();
@@ -92,15 +94,15 @@ public final class FindMeetingQuery {
       }
     }
 
-    List<Set<String>> setsOfIptionals = new ArrayList<>();
+    List<Set<String>> setOfBusyOptionalsEachMinute = new ArrayList<>();
     for (int i = 0; i <= TimeRange.END_OF_DAY; i++) {
-      setsOfIptionals.add(new HashSet<>());
+      setOfBusyOptionalsEachMinute.add(new HashSet<>());
     }
 
     // find busy people set for every minute and merge sets if there are overlaping time ranges
     for (TimeRange timeRange : busyTimesForOptionals.keySet()) {
       for (int i = timeRange.start(); i < timeRange.end(); i++) {
-        setsOfIptionals.get(i).addAll(busyTimesForOptionals.get(timeRange));
+        setOfBusyOptionalsEachMinute.get(i).addAll(busyTimesForOptionals.get(timeRange));
       }
     }
 
@@ -108,17 +110,15 @@ public final class FindMeetingQuery {
     int totalOptionals = optionalAttendees.size();
 
     for (int i = 0; i <= TimeRange.END_OF_DAY; i++) {
-      numberOfFreeOptionals.add(totalOptionals - setsOfIptionals.get(i).size());
+      numberOfFreeOptionals.add(totalOptionals - setOfBusyOptionalsEachMinute.get(i).size());
     }
-
-    numberOfFreeOptionals.add(-1);
 
     return numberOfFreeOptionals;
   }
 
   /**
-   * Calculates for each number of optional attendees, chich free time ranges are overlaping with
-   * available times for required attendees and returns overlaping free times with higest number
+   * Calculates for each number of optional attendees, which free time ranges are overlaping with
+   * available times for required attendees and returns overlaping free times with highest number
    * possible
    */
   private Collection<TimeRange> findOverlaps(
@@ -139,25 +139,30 @@ public final class FindMeetingQuery {
       int start = availableTime.start();
       int end = availableTime.end();
 
-      int number = numberOfOptionals.get(start);
+      if (end > TimeRange.END_OF_DAY) {
+        end = TimeRange.END_OF_DAY;
+      }
+      int optionalsNum = numberOfOptionals.get(start);
 
       // for time range which has same number of free optionals
       int localStart = start;
       int localEnd = start;
 
       for (int i = start; i <= end; i++) {
-        if (numberOfOptionals.get(i) != number || i == end) {
+        if (numberOfOptionals.get(i) != optionalsNum || i == end) {
           TimeRange time;
+
           if (i == TimeRange.END_OF_DAY) {
             time = TimeRange.fromStartEnd(localStart, localEnd, true);
           } else {
             time = TimeRange.fromStartEnd(localStart, localEnd, false);
           }
+
           if (localEnd - localStart >= duration) {
-            overlaps.get(number).add(time);
+            overlaps.get(optionalsNum).add(time);
           }
 
-          number = numberOfOptionals.get(i);
+          optionalsNum = numberOfOptionals.get(i);
           localStart = i;
         }
 
@@ -175,34 +180,33 @@ public final class FindMeetingQuery {
   }
 
   /* Find the free time period of at least event length using busy time ranges */
-  private Collection<TimeRange> findFreeTimesFromBusy(
-      Collection<TimeRange> notAvailableTimes, int duration) {
-    int start = TimeRange.START_OF_DAY;
-    int end = TimeRange.END_OF_DAY;
+  private Collection<TimeRange> findFreeTimesFromBusy(List<TimeRange> busyTimes, int duration) {
+    Collections.sort(busyTimes, TimeRange.ORDER_BY_START);
 
+    int start = TimeRange.START_OF_DAY;
     List<TimeRange> availableTimes = new ArrayList<TimeRange>();
 
-    for (TimeRange timeRange : notAvailableTimes) {
+    for (TimeRange timeRange : busyTimes) {
       int timeRangeStart = timeRange.start();
       int timeRangeEnd = timeRange.end();
 
-      // |--------------|
-      //     |------|
+      // free time: |--------------|
+      // busy time:    |------|
       if (timeRangeStart > start) {
         if (timeRangeStart - start >= duration) {
           availableTimes.add(TimeRange.fromStartEnd(start, timeRangeStart, false));
         }
         start = timeRangeEnd;
       } else
-      //     |---------|
-      // |------|
-      if (timeRangeStart <= start && timeRangeEnd > start) {
+      // free time:    |---------|
+      // busy time: |------|
+      if (timeRangeEnd > start) {
         start = timeRangeEnd;
       }
     }
 
-    if (end - start >= duration) {
-      availableTimes.add(TimeRange.fromStartEnd(start, end, true));
+    if (TimeRange.END_OF_DAY - start >= duration) {
+      availableTimes.add(TimeRange.fromStartEnd(start, TimeRange.END_OF_DAY, true));
     }
 
     return availableTimes;
